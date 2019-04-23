80b6d602c810b121827f41bea982d14327398c24
T_c <miguelito.vargasf@gmail.com>
2019-04-23 09:46:42 -0500

    started playground for threejs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import ThreeSimple from './components/threeSimple'
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,18 +8,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="testing" />
+        <ThreeSimple />
       </header>
     </div>
   );
